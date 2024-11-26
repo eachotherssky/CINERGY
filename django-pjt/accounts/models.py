d27a16fc2f485a0cd86368b6be8d783e55da2a2d
Park <psangho76@gmail.com>
2024-11-27 01:38:37 +0900

    re-reset

--- django-pjt/accounts/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-    kakao_id = models.CharField(max_length=100, null=True, blank=True)
-    profile_image = models.URLField(null=True, blank=True)
-    followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
